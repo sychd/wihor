1783f25afdb696668aaa0cd7e5704eb79a8825f2
Denys Sych <denis.feelin@gmail.com>
2024-01-25 09:22:15 +0100

    added simple routes; added tailwind and daisyUI

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,14 @@
+<h1 class="text-3xl font-bold">
+	Hello!
+</h1>
+
+<nav>
+	<a href="/settings">settings</a>
+	<a href="/session">session</a>
+</nav>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/routes/**/*.{svelte,js,ts}'],
+	plugins: [require('daisyui')],
+	theme: {
+		extend: {}
+	}
+};
